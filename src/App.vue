c7b96a9d8d2e1ac99b074a3b57269733c3327119
Jestric-sys <Poltos_94@mail.ru>
2020-09-06 00:04:04 +0300

    add category

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <div id="app">
+  <div id="app">    
     <router-view/>
   </div>
 </template>
 
-<style>
-
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
