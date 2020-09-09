8cc9ec2e2883d4e33b0a08eb8800e46a38e12108
AlexandrA <alexag488@mail.com>
2020-09-09 15:58:15 +0300

    Добавлен следующий функционал: •	Регистрация •	Авторизация login\logoff •	Получение личного профиля, редактирование профиля, сброс пароля

--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,35 @@
-
+//file version 1.1.0
 const serverConfig = require("./src/config/server.config");  
 const express = require('express'); 
 const app = express();
+const passport = require('passport');
+const session = require('express-session');
+
+// Passport Config
+require('./src/auth/passport')(passport);
 
 app.use(express.json());  
 app.use(express.urlencoded({ extended: true }));
-    
+
+ // Express session
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+  
+  // Passport middleware
+  app.use(passport.initialize());
+  app.use(passport.session());   
 //---
-app.get("/", async (req, res) => {
-    res.json({ message: "root директория /" });
+app.get("/api/v1", async (req, res) => {
+    res.json({ message: "API v1" });
 });
 
 require("./src/routers/routers")(app);
 
 app.listen(serverConfig.PORT, () => {
-    console.log(`[1] - Сервер запущен на ${serverConfig.PORT} порту`);
+    console.log(`[1] - Server start on port: ${serverConfig.PORT}`);
 });
-
-
-
-
-
-
--- Backend/src/routers/routers.js
+++ Backend/src/routers/routers.js
@@ -1,27 +1,142 @@
+//file version 1.1.0
+const passport = require('passport');
+const { UI } = require('../config/server.config');
+const path = require('path');
+
+//validate Empty JSON
+const validateEmptyJSON = (req, res, next) => {
+  console.log(req.body);
+    console.log(`Get keys JSON:\n${Object.keys(req.body)}`);
+      if(Object.keys(req.body).length === 0) {
+        return res.status(400).send({
+            error: "Пустое значение!"
+        });
+      };
+    next();
+};
+
 module.exports = app => {
   const customers = require("../controllers/conrollers");
-  //Функция проверки на пустой json
-  const validateEmptyJSON = (req, res, next) => {
-      console.log(`Получены ключи JSON:\n${Object.keys(req.body)}`);
-        if(Object.keys(req.body).length === 0) {
-          return res.status(400).send({
-              error: "Пустое значение!"
-          });
-        };
-      //console.log(`Получены данные:\n${req.body}`);
-      next();
+  function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    if(UI){
+      res.redirect('/api/v1/login');
+    } else{
+      res.status(401).json({ message: "Unauthorized" }); 
+    };   
+  };
+  
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/api/v1/index')
+    }
+    next()
   };
 
-  //Регистрация пользователя
-  app.post("/register/user", validateEmptyJSON, customers.newUser);
-  //Регистрация мастера
-  app.post("/register/master", validateEmptyJSON, customers.newMaster);
-  // получить всех мастеров
-  app.get("/master", customers.findAllMasters);
-  //Карточка мастера
-  app.get("/master/:customerId", customers.findOne);
-  //список категорий
-  app.get("/search", customers.getCategory);
-  app.get("/citys", customers.getCitys);
+  // Authentication
+  app.get("/api/v1/login", checkNotAuthenticated, (req,res)=>{
+    if(UI){
+      res.sendFile(path.resolve(__dirname, '../../html/login.html'));
+    } else{
+      res.json({ message: "/api/v1/login" }); 
+    };
+  });
+
+  app.post('/api/v1/login', checkNotAuthenticated, function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+      if(UI)  {
+        if (err) { return next(err); };
+        if (!user) { return res.redirect('/api/v1/login'); };
+        req.logIn(user, function(err) {
+        if (err) { return next(err); };
+        return res.redirect('/api/v1/index');
+        });
+      } else {
+          if (err) { return next(err); };
+          if (!user) { return res.status(401); };
+          req.logIn(user, function(err) {
+          if (err) { return next(err); };
+          return res.status(200).json(info);
+        }); 
+      };
+    })(req, res, next);
+  });
+
+  // Logout
+  app.get('/api/v1/logout',checkAuthenticated, (req, res) => {
+    req.logout();
+    if(UI){
+      res.redirect('/api/v1/login');
+    } else{
+      res.status(200).json({ message: "logget off" }); 
+    };
+  });
+    
+  //Register
+  app.get("/api/v1/register", checkNotAuthenticated, (req,res)=>{
+    if(UI){
+      res.sendFile(path.resolve(__dirname, '../../html/register.html'));
+    } else{
+      res.json({ message: "/api/v1/register" }); 
+    };
+  });
+
+  app.post("/api/v1/register", validateEmptyJSON, customers.newUser);
+
+  //User pages
+  app.get("/api/v1/index", checkAuthenticated, (req,res)=>{
+    if(UI){
+      res.sendFile(path.resolve(__dirname, '../../html/index.html'));
+    } else{
+      res.json({ message: "/api/v1/index" }); 
+    };
+    
+  });
+
+  //get customers profile
+  app.get("/api/v1/profile", checkAuthenticated,  customers.getCustomerProfile);
+
+  //update user profile
+  //UI for test
+  app.get("/api/v1/profile/userupdate", checkAuthenticated, (req,res)=>{
+    if(UI){
+      res.sendFile(path.resolve(__dirname, '../../html/userupdate.html'));
+    } else{
+      res.json({ message: "/api/v1/userupdate" }); 
+    };
+  });
+
+  app.post("/api/v1/profile/userupdate", checkAuthenticated,  customers.updateUserProfile);
+
+  //update master profile
+  //UI for test
+  app.get("/api/v1/profile/masterupdate", checkAuthenticated, (req,res)=>{
+    if(UI){
+      res.sendFile(path.resolve(__dirname, '../../html/masterupdate.html'));
+    } else{
+      res.json({ message: "/api/v1/masterupdate" }); 
+    };
+  });
+
+  //update master profile
+  app.post("/api/v1/profile/masterupdate", checkAuthenticated,  customers.updateMasterProfile);
+  //update master jobs profile
+  app.post("/api/v1/profile/masterupdate/jobs", checkAuthenticated,  customers.updateMasterProfileJobs);
+  //reset password
+  app.post("/api/v1/profile/resetpwd", checkAuthenticated,  customers.resetPwd);
+
+  //Get All masters
+  app.get("/api/v1/master", checkAuthenticated, customers.findAllMasters);
+
+  //master info card
+  app.get("/api/v1/master/:customerId",checkAuthenticated, customers.findOne);
+
+  //category list
+  app.get("/api/v1/search", checkAuthenticated, customers.getCategory);
+
+  //citys list
+  app.get("/api/v1/citys", checkAuthenticated, customers.getCitys);
 };
 
